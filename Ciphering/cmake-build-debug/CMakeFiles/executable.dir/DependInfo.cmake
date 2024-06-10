7385e4dd7085dc99163a2186b2429dc87a777588
chkokof <157061442+chkokof@users.noreply.github.com>
2024-06-09 18:25:01 -0600

    changes

---
+++ Ciphering/cmake-build-debug/CMakeFiles/executable.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/charleskokofi/Desktop/JobHub/MyIdeas/midterm/main.c" "CMakeFiles/executable.dir/main.c.o" "gcc" "CMakeFiles/executable.dir/main.c.o.d"
+  "/Users/charleskokofi/Desktop/JobHub/MyIdeas/midterm/percent.c" "CMakeFiles/executable.dir/percent.c.o" "gcc" "CMakeFiles/executable.dir/percent.c.o.d"
+  "/Users/charleskokofi/Desktop/JobHub/MyIdeas/midterm/substitution.c" "CMakeFiles/executable.dir/substitution.c.o" "gcc" "CMakeFiles/executable.dir/substitution.c.o.d"
+  "/Users/charleskokofi/Desktop/JobHub/MyIdeas/midterm/test_percent.c" "CMakeFiles/executable.dir/test_percent.c.o" "gcc" "CMakeFiles/executable.dir/test_percent.c.o.d"
+  "/Users/charleskokofi/Desktop/JobHub/MyIdeas/midterm/test_substitution.c" "CMakeFiles/executable.dir/test_substitution.c.o" "gcc" "CMakeFiles/executable.dir/test_substitution.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Ciphering/cmake-build-debug/CMakeFiles/executable.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/executable.dir/main.c.o"
+  "CMakeFiles/executable.dir/main.c.o.d"
+  "CMakeFiles/executable.dir/percent.c.o"
+  "CMakeFiles/executable.dir/percent.c.o.d"
+  "CMakeFiles/executable.dir/substitution.c.o"
+  "CMakeFiles/executable.dir/substitution.c.o.d"
+  "CMakeFiles/executable.dir/test_percent.c.o"
+  "CMakeFiles/executable.dir/test_percent.c.o.d"
+  "CMakeFiles/executable.dir/test_substitution.c.o"
+  "CMakeFiles/executable.dir/test_substitution.c.o.d"
+  "executable"
+  "executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
